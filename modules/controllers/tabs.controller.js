dba0b8378a5a079839415f3fb0830367de278e0b
Dalpatov <dalpatov.exceedteam@gmail.com>
2021-04-22 09:55:44 +0300

    create tasks6.Create a new Tab

--- modules/controllers/tabs.controller.js
+++ modules/controllers/tabs.controller.js
@@ -1,11 +1,16 @@
 const Tabs = require('../../db/models/users/tabs');
 
 module.exports.allTabs = async (req, res, next) => {
-  const tabs = new Tabs(req.body);
-    tabs.save().then(result => {
-      Tabs.find().then(result => {
-        res.send({data:result});
-      });
-    });
+  Tabs.find().then((result) => {
+    res.send({ data: result });
+  });
 };
 
+module.exports.createTabs = async (req, res, next) => {
+ const tabs = new Tabs(req.body);
+    tabs.save().then(result => {
+      Tabs.find().then(result => {
+        res.send(result);
+      }).catch(err => res.status(404).send({error: "unable to create new table"}))
+    });
+}
